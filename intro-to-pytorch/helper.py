3aa285bac0b00d0faa0dab2ed05551751e24d6f1
Michael Virgo <michael.v@udacity.com>
2019-05-28 13:24:26 -0700

    Docs: note dataset location in workspace

--- intro-to-pytorch/helper.py
+++ intro-to-pytorch/helper.py
@@ -77,6 +77,8 @@ def view_classify(img, ps, version="MNIST"):
     ax2.set_yticks(np.arange(10))
     if version == "MNIST":
         ax2.set_yticklabels(np.arange(10))
+    elif version == "CAT_DOG":
+        ax2.set_yticklabels(np.arange(2))        
     elif version == "Fashion":
         ax2.set_yticklabels(['T-shirt/top',
                             'Trouser',
@@ -92,3 +94,38 @@ def view_classify(img, ps, version="MNIST"):
     ax2.set_xlim(0, 1.1)
 
     plt.tight_layout()
+
+    
+    
+def view_classify2(img, ps, version="MNIST"):
+    ''' Function for viewing an image and it's predicted classes.
+    '''
+    ps = ps.data.numpy().squeeze()
+
+    fig, (ax1, ax2) = plt.subplots(figsize=(6,9), ncols=2)
+    ax1.imshow(img.resize_(3, 224, 224).numpy().squeeze())
+    ax1.axis('off')
+    ax2.barh(np.arange(10), ps)
+    ax2.set_aspect(0.1)
+    ax2.set_yticks(np.arange(10))
+    if version == "MNIST":
+        ax2.set_yticklabels(np.arange(10))
+    elif version == "CAT_DOG":
+        ax2.set_yticklabels(np.arange(2))        
+    elif version == "Fashion":
+        ax2.set_yticklabels(['T-shirt/top',
+                            'Trouser',
+                            'Pullover',
+                            'Dress',
+                            'Coat',
+                            'Sandal',
+                            'Shirt',
+                            'Sneaker',
+                            'Bag',
+                            'Ankle Boot'], size='small');
+    ax2.set_title('Class Probability')
+    ax2.set_xlim(0, 1.1)
+
+    plt.tight_layout()
+    
+    
